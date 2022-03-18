bca9b84c8e033272638f710f82569b362d382337
Emily Pensinger <emily.pensinger@gmail.com>
2022-03-18 13:26:31 -0400

    Create AddingNumbers.cpp

---
+++ AddingNumbers.cpp
@@ -0,0 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int num1;
+    int num2;
+    int sum;
+    
+    cout<<"Please enter two numbers separated by a space:"<<endl;
+    cin>>num1>>num2;
+    
+    sum = num1 + num2;
+    
+    cout<<num1<<" + "<<num2<<" = "<<sum<<endl;
+    
+
+    return 0;
+}
