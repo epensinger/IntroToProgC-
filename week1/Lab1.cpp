362df4286453f6634529888bc9e047502b04523f
Emily Pensinger <emily.pensinger@gmail.com>
2022-03-18 13:32:16 -0400

    Create Lab1.cpp

---
+++ week1/Lab1.cpp
@@ -0,0 +1,22 @@
+//Description
+
+//Write a program that prints Hello World
+
+//File Name 
+
+//hello.cpp
+
+//Score
+
+//There is a single test worth 2 points
+
+//Solution:
+
+#include<iostream>
+using namespace std;
+
+int main() {
+  cout<<"Hello World";
+  return 0;
+  
+}
