586b26f123c0efa5647bff18e1826a4ded0f8fd3
Emily Pensinger <emily.pensinger@gmail.com>
2022-04-14 09:06:26 -0400

    Create AddingNumbers.py
    Example of the same program in python

---
+++ week1/AddingNumbers.py
@@ -0,0 +1,8 @@
+#This program reads two integers from the user and prints their sum
+
+num1 = input("Please enter a number")
+num2 = input("Please enter a number")
+
+sum = int(num1) + int(num2)
+
+print(sum)
